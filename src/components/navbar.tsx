691d76d4569286722334539dbc158728c9669bb6
Aman Raj Rana <amanrajrana.dev@gmail.com>
2024-05-06 21:55:51 +0530

    Config ShadCN ui

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 export default function NavBar() {
   return (
-    <nav>
-        
-    </nav>
-  )
+    <div className="border-b-2 border-border-primary">
+      <nav className="container flex justify-between items-center py-5">
+        <div>
+          <Image
+            src={"./logo.svg"}
+            className="h-[37px]"
+            alt="logo"
+            height={37}
+            width={80}
+          />
+        </div>
+        <Link href={"/signup"} className="text-primary text-xl font-bold">
+          Sign Up
+        </Link>
+      </nav>
+    </div>
+  );
 }
