2e87b52deed18a98c32e83debced5ac02d532bb5
Aman Raj Rana <amanrajrana.dev@gmail.com>
2024-05-10 19:56:30 +0530

    Upgraded Next.js app to PWA for offline functionality and improved performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,59 @@
-import type { Metadata } from "next";
-import { Inter, Poppins } from "next/font/google";
+import type { Metadata, Viewport } from "next";
+import { Poppins } from "next/font/google";
 import "@/app/global.css";
 import UserState from "@/context/user/userState";
 
+// APP Metadata constants
+const APP_NAME = "Trillion Miles";
+const APP_DEFAULT_TITLE = "Trillion Miles";
+const APP_TITLE_TEMPLATE = "%s - Trillion Miles";
+const APP_DESCRIPTION = "Journey to a trillion miles starts from here!!";
+
+// APP Font constants
 const poppins = Poppins({
   weight: ["200", "300", "400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
+// APP Viewport export
+export const viewport: Viewport = {
+  themeColor: "#00B2FF",
+};
+
+// APP Metadata and required metadata for PWA
 export const metadata: Metadata = {
-  title: "Trillion Mile Journey",
-  description: "Journey to a trillion miles starts from here!!",
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  applicationName: APP_NAME,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 export default function RootLayout({
